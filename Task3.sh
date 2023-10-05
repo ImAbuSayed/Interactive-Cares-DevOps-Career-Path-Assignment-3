f06611c996cf883319a54548521256dc92c19a3f
Abu Sayed <66951173+ImAbuSayed@users.noreply.github.com>
2023-10-06 00:45:37 +0600

    Create Task3.sh
    The instruction was: - Create two files on each directory

---
+++ Task3.sh
@@ -0,0 +1,4 @@
+sudo touch /home/AbuSayed/os-concepts/file1 /home/AbuSayed/os-concepts/file2
+sudo touch /home/AbuSayed/ubuntu-is-the-best/file3 /home/AbuSayed/ubuntu-is-the-best/file4
+sudo ls /home/AbuSayed/os-concepts
+sudo ls /home/AbuSayed/ubuntu-is-the-best
