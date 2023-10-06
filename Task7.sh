aee06c26dc2a209a816d06d60486cd9608067fd8
Abu Sayed <66951173+ImAbuSayed@users.noreply.github.com>
2023-10-06 06:51:13 +0600

    Create Task7.sh
    The instruction was: - Update file ownership to the newly created group

---
+++ Task7.sh
@@ -0,0 +1,4 @@
+sudo chown :SayedsGroup /home/AbuSayed/os-concepts/*
+sudo chown :SayedsGroup /home/AbuSayed/ubuntu-is-the-best/*
+sudo ls -l /home/AbuSayed/ubuntu-is-the-best
+sudo ls -l /home/AbuSayed/os-concepts
